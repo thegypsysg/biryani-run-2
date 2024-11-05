8ef330c5a1bbda3598760e4e0de123b8b0ef23f1
Ma. Eden Ramoneda <edenlee143@gmail.com>
2024-11-06 00:04:02 +0800

    Added slide container

--- src/components/home/delivery-biryani.vue
+++ src/components/home/delivery-biryani.vue
@@ -11,8 +11,48 @@
             
             :items="['Marine Parade']"
             ></v-autocomplete>
+
+            <Carousel v-bind="settings" :breakpoints>
+                <Slide v-for="(item, i) in 5" :key="i">
+                    
+                </Slide>
+
+                <template #addons>
+                    <Navigation />
+                    <Pagination />
+                </template>
+            </Carousel>
         </div>
       
-        
+      
     </div>
 </template>
+
+<script lang="ts" setup>
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
+
+const settings: breakpoints = {
+  itemsToShow: 1,
+  snapAlign: 'center',
+};
+
+const breakpoints = {
+  // 200px and up
+  200: {
+    itemsToShow: 1,
+    snapAlign: 'center',
+  },
+  // 640px and up
+  640: {
+    itemsToShow: 2,
+    snapAlign: 'center',
+  },
+  // 700px and up
+  960: {
+    itemsToShow: 4,
+    snapAlign: 'center',
+  },
+};
+
+</script>
