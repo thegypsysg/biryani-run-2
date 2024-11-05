cd5ceb4c95f797667a68a63f03a4c2cf2257947d
Ma. Eden Ramoneda <edenlee143@gmail.com>
2024-11-06 00:09:40 +0800

    Added slider for town

--- src/components/home/delivery-biryani.vue
+++ src/components/home/delivery-biryani.vue
@@ -12,9 +12,16 @@
             :items="['Marine Parade']"
             ></v-autocomplete>
 
-            <Carousel v-bind="settings" :breakpoints>
+            <Carousel v-bind="settings" :breakpoints class="pt-8">
                 <Slide v-for="(item, i) in 5" :key="i">
-                    
+                    <div class=" d-flex border pa-2 rounded-lg ">
+                
+                   
+                        <v-img aspect-ratio="1" height="40" width="40" cover src="https://images.unsplash.com/photo-1525625293386-3f8f99389edd?q=80&w=1952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></v-img>
+                 
+                    <p class="text-no-wrap d-flex align-center pl-2">Marine Parade</p>
+
+                    </div>
                 </Slide>
 
                 <template #addons>
