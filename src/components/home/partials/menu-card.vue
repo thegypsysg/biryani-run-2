51eeb0b02b804103472058f5d12784988aa85d86
Ma. Eden Ramoneda <edenlee143@gmail.com>
2024-11-06 22:47:43 +0800

    changed the displaying of partner name in menu-card

--- src/components/home/partials/menu-card.vue
+++ src/components/home/partials/menu-card.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="bg-white pa-2 border ma-2" elevation="4" style="border-radius: 10px;">
+    <div class="d-flex justify-start align-center ga-3">
+        <v-img :src="props.fileURL + props.menu.restaurant?.partner?.logo" aspect-ratio="1"
+          style="width: 35px; height: 35px; max-width: 35px; max-height: 35px;"></v-img>
+        <div class="font-weight-black text-body">{{ props.menu.restaurant?.partner ? props.menu.restaurant?.partner.partner_name : '' }}</div>
+      </div>
     <div class="d-flex justify-space-between align-center font-weight-bold text-body  pa-2">
       <p class="text-red-darken-2">{{ props.menu.actual_dish_name || props.menu.dish?.dish_name }}</p>
       <p class="text-red-lighten-3">{{ props.menu.price }}</p>
     </div>
     <v-img :src="props.fileURL + props.menu.main_image" cover aspect-ratio="1"></v-img>
     <div class="pa-0">
-      <div class="d-flex justify-start align-center ga-3">
-        <v-img :src="props.fileURL + props.menu.restaurant?.partner?.logo" aspect-ratio="1"
-          style="width: 35px; height: 35px; max-width: 35px; max-height: 35px;"></v-img>
-        <div class="font-weight-black text-body">{{ props.menu.restaurant?.partner ? props.menu.restaurant?.partner.partner_name : '' }}</div>
-      </div>
+    
 
       <div class="font-weight-light text-body-1 text-justify">{{ props.menu.dish_description }}</div>
 
