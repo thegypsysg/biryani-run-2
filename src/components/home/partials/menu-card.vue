e2b8c1198c6cffc95d0324f825943180ea517911
Ma. Eden Ramoneda <edenlee143@gmail.com>
2024-11-06 22:56:36 +0800

    Fixed the displaying of partner name and address of resto

--- src/components/home/partials/menu-card.vue
+++ src/components/home/partials/menu-card.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="bg-white pa-2 border ma-2" elevation="4" style="border-radius: 10px;">
-    <div class="d-flex justify-start align-center ga-3">
+    <div class="d-flex justify-start align-center text-start ga-3">
         <v-img :src="props.fileURL + props.menu.restaurant?.partner?.logo" aspect-ratio="1"
           style="width: 35px; height: 35px; max-width: 35px; max-height: 35px;"></v-img>
-        <div class="font-weight-black text-body">{{ props.menu.restaurant?.partner ? props.menu.restaurant?.partner.partner_name : '' }}</div>
+        <div class="text-body">
+          <div class="font-weight-black ">{{ props.menu.restaurant?.partner ? props.menu.restaurant?.partner.partner_name : '' }}</div>
+          <div class="text-blue-grey-lighten-2 text-subtitle-2">{{ props.menu.restaurant?.partner?.address ? props.menu.restaurant?.partner.address : 'Marine Parade' }}</div>
+        </div>
+     
       </div>
     <div class="d-flex justify-space-between align-center font-weight-bold text-body  pa-2">
       <p class="text-red-darken-2">{{ props.menu.actual_dish_name || props.menu.dish?.dish_name }}</p>
