14fa2e2413aa89f7227b6fd123c3241411c26527
Ma. Eden Ramoneda <edenlee143@gmail.com>
2024-11-06 23:27:14 +0800

    displayed rating in menu

--- src/components/home/partials/menu-card.vue
+++ src/components/home/partials/menu-card.vue
@@ -1,29 +1,38 @@
 <template>
-  <div class="bg-white pa-2 border ma-2" elevation="4" style="border-radius: 10px;">
+  <div class="bg-white pa-2 ma-2" elevation="4" style="width: 100%; height: 100%;">
     <div class="d-flex justify-start align-center text-start ga-3">
         <v-img :src="props.fileURL + props.menu.restaurant?.partner?.logo" aspect-ratio="1"
           style="width: 35px; height: 35px; max-width: 35px; max-height: 35px;"></v-img>
-        <div class="text-body">
-          <div class="font-weight-black ">{{ props.menu.restaurant?.partner ? props.menu.restaurant?.partner.partner_name : '' }}</div>
-          <div class="text-blue-grey-lighten-2 text-subtitle-2">{{ props.menu.restaurant?.partner?.address ? props.menu.restaurant?.partner.address : 'Marine Parade' }}</div>
+        <div>
+          <div class="font-weight-black text-subtitle-1">{{ props.menu.restaurant?.partner ? props.menu.restaurant?.partner.partner_name : '' }}</div>
+          <div class="text-blue-grey-lighten-2 text-caption">{{ props.menu.restaurant?.partner?.address ? props.menu.restaurant?.partner.address : 'Marine Parade' }}</div>
         </div>
      
       </div>
-    <div class="d-flex justify-space-between align-center font-weight-bold text-body  pa-2">
-      <p class="text-red-darken-2">{{ props.menu.actual_dish_name || props.menu.dish?.dish_name }}</p>
-      <p class="text-red-lighten-3">{{ props.menu.price }}</p>
-    </div>
+  
     <v-img :src="props.fileURL + props.menu.main_image" cover aspect-ratio="1"></v-img>
     <div class="pa-0">
     
+      <div class="d-flex justify-space-between align-center font-weight-bold text-body pa-2">
+      <p class="font-weight-black text-subtitle-2  text-start">{{ props.menu.actual_dish_name || props.menu.dish?.dish_name }}</p>
+      <v-rating
+        :length="5"
+        :size="20"
+        :model-value="4"
+          density="comfortable"
+        class="text-red"
+        active-color="text-red"
+      />
+    </div>
+      <!-- <div class="font-weight-light text-body-1 text-justify">{{ props.menu.dish_description }}</div> -->
 
-      <div class="font-weight-light text-body-1 text-justify">{{ props.menu.dish_description }}</div>
-
-      <hr class="my-2">
+      <!-- <hr class="my-2">
       <div class="text-caption text-left">
         <span class="font-weight-bold text-blue-darken-3">{{ props.menu.views }}</span>
         Purchases | {{ 300}} Views
-      </div>
+      </div> -->
+
+
     </div>
   </div>
 </template>
